b2fd0a9c2923c88d0b9b1ebbb7f0a7c4013c5062
Michał Kałuziński <mi.kaluzinski@gmail.com>
2016-08-23 13:45:19 +0200

    - create class Network

--- NeuralNetworkXor/NeuralNetworkXor/Network.cs
+++ NeuralNetworkXor/NeuralNetworkXor/Network.cs
@@ -8,5 +8,157 @@ namespace NeuralNetworkXor
 {
     class Network
     {
+        int numberInputs;
+        int numberOutputs;
+        int numberHidden;
+        int numberTraining;
+        float learningrate = 0.005f;
+
+        List<List<float>> inputs;
+        List<List<float>> outputs;
+        float[,] Wxh;
+        float[,] Why;
+
+        public Network(int numberInputs, int numberOutputs, int numberHidden, int numberTraining, List<List<float>> inputs, List<List<float>> outputs, float[,] Wxh, float[,] Why)
+        {
+            this.numberInputs = numberInputs;
+            this.numberHidden = numberHidden;
+            this.numberOutputs = numberOutputs;
+            this.numberTraining = numberTraining;
+            this.inputs = inputs;
+            this.outputs = outputs;
+
+            this.Wxh = new float[numberInputs + 1, numberHidden];
+            this.Why = new float[numberHidden + 1, numberOutputs];
+            this.Wxh = Wxh;
+            this.Why = Why;
+        }
+
+        public void learning(int iterations, bool random)
+        {
+            float[] x = new float[numberInputs + 1];
+            float[] y = new float[numberOutputs];
+            float[] zhWeightedSums = new float[numberHidden];
+            float[] hActivationValues = new float[numberHidden + 1];
+            float[] zyWeightedSums = new float[numberOutputs];
+            float[] probabilities = new float[numberOutputs];
+            float[] outputErrors = new float[numberOutputs];
+            float[,] deltaWxh = new float[numberInputs + 1, numberHidden];
+            float[,] deltaWhy = new float[numberHidden + 1, numberOutputs];
+            float loss, sum; //for storing the loss
+            int t, i, h, o;
+            Random randomTraining = new Random();
+            int currentInputTraining;
+
+            for (t = 0; t < iterations; t++)
+            {
+
+                if (random)
+                    currentInputTraining = randomTraining.Next(0, numberTraining);
+                else
+                    currentInputTraining = t % numberTraining;
+
+                for (i = 1; i < numberInputs; i++)
+                    x[i] = Convert.ToSingle(inputs[currentInputTraining][i]);
+                x[0] = 1;
+
+                for (i = 0; i < numberOutputs; i++)
+                    y[i] = outputs[currentInputTraining][i];
+
+
+
+                for (i = 0; i < numberHidden; i++)
+                    zhWeightedSums[i] = 0;
+                for (h = 0; h < numberHidden; h++) for (i = 0; i < numberInputs + 1; i++) zhWeightedSums[h] += x[i] * Wxh[i, h];
+
+                hActivationValues[0] = 1;
+                for (h = 0; h < numberHidden; h++) hActivationValues[h + 1] = Convert.ToSingle(Math.Tanh(zhWeightedSums[h]));
+
+                for (i = 0; i < numberOutputs; i++)
+                    zyWeightedSums[i] = 0;
+
+                for (o = 0; o < numberOutputs; o++) for (h = 0; h < numberHidden + 1; h++) zyWeightedSums[o] += hActivationValues[h] * Why[h, o];
+
+                for (sum = 0, o = 0; o < numberOutputs; o++) { probabilities[o] = Convert.ToSingle(Math.Exp(zyWeightedSums[o])); sum += probabilities[o]; }
+
+             
+
+                for (o = 0; o < numberOutputs; o++) outputErrors[o] = probabilities[o] - y[o];
+
+                for (loss = 0, o = 0; o < numberOutputs; o++) loss -= y[o] * Convert.ToSingle(Math.Log(probabilities[o]));
+             
+                for (h = 0; h < numberHidden + 1; h++) for (o = 0; o < numberOutputs; o++) deltaWhy[h, o] = hActivationValues[h] * outputErrors[o];
+
+
+                for (i = 0; i < numberHidden + 1; i++)
+                    hActivationValues[i] = 0;
+
+                for (h = 1; h < numberHidden + 1; h++) for (o = 0; o < numberOutputs; o++) hActivationValues[h] += Why[h, o] * outputErrors[o];
+
+                for (h = 0; h < numberHidden; h++) zhWeightedSums[h] = hActivationValues[h + 1] * (1 - Convert.ToSingle(Math.Pow(Math.Tanh(zhWeightedSums[h]), 2)));
+
+
+                for (i = 0; i < numberInputs + 1; i++) for (h = 0; h < numberHidden; h++) deltaWxh[i, h] = x[i] * zhWeightedSums[h];
+
+
+                for (h = 0; h < numberHidden + 1; h++) for (o = 0; o < numberOutputs; o++) Why
+                [h, o] -= learningrate * deltaWhy[h, o];
+                for (i = 0; i < numberInputs + 1; i++) for (h = 0; h < numberHidden; h++) Wxh[i, h] -= learningrate * deltaWxh[i, h];
+
+            }
+        }
+
+        public List<List<float>> estimate(List<List<float>> inputs)
+        {
+            List<List<float>> allProbabilities = new List<List<float>>();
+
+            float[] x = new float[numberInputs + 1];
+            float[] y = new float[numberOutputs];
+            float[] zhWeightedSums = new float[numberHidden];
+            float[] hActivationValues = new float[numberHidden + 1];
+            float[] zyWeightedSums = new float[numberOutputs];
+            float[] probabilities = new float[numberOutputs];
+            float sum; //for storing the loss
+            int t, i, h, o;
+
+
+            for (t = 0; t < inputs.Count; t++)
+            {
+                for (i = 1; i < numberInputs; i++)
+                    x[i] = Convert.ToSingle(inputs[t][i]);
+                x[0] = 1;
+
+                for (i = 0; i < numberOutputs; i++)
+                    y[i] = outputs[t][i];
+
+
+
+                for (i = 0; i < numberHidden; i++)
+                    zhWeightedSums[i] = 0;
+                for (h = 0; h < numberHidden; h++) for (i = 0; i < numberInputs + 1; i++) zhWeightedSums[h] += x[i] * Wxh[i, h];
+
+                hActivationValues[0] = 1;
+                for (h = 0; h < numberHidden; h++) hActivationValues[h + 1] = Convert.ToSingle(Math.Tanh(zhWeightedSums[h]));
+
+                for (i = 0; i < numberOutputs; i++)
+                    zyWeightedSums[i] = 0;
+
+                for (o = 0; o < numberOutputs; o++) for (h = 0; h < numberHidden + 1; h++) zyWeightedSums[o] += hActivationValues[h] * Why[h, o];
+
+                List<float> currentProbabilities = new List<float>();
+                for (sum = 0, o = 0; o < numberOutputs; o++) { probabilities[o] = Convert.ToSingle(Math.Exp(zyWeightedSums[o])); sum += probabilities[o];
+                currentProbabilities.Add(probabilities[o]);
+                }
+
+                allProbabilities.Add(currentProbabilities.ToList());
+            
+            }
+            return allProbabilities;
+        }
+
+        public void setLearningRate(float learningRate)
+        {
+            this.learningrate = learningRate;
+        }
     }
 }
--- NeuralNetworkXor/NeuralNetworkXor/Program.cs
+++ NeuralNetworkXor/NeuralNetworkXor/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading;
+using NeuralNetworkXor;
 
 namespace Test
 {
@@ -10,145 +11,94 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            int numberInputs = 5;
-            int numberHidden = 5;
-            int numberOutputs = 5;
-            int numberTraining = 10;
-
+            int numberInputs = 2;
+            int numberHidden = 10;
+            int numberOutputs = 2;
+            int numberTraining = 4;
+            int i;
             Random random = new Random();
-            float[,] inputs = new float[,] {{10,20,3,4}, {18,77,8,6}, {5,4,6,6}, {-1,-8,-3,-5}};
-            float[,] outputs = new float[,] {{1,0}, {1,0}, {1,0}, {0,1}};
-            float learningrate = 0.05f;
-            int iterations = 10000;
-
-
-
-            float[,] Wxh = new float[numberInputs+1, numberHidden];
+            float[,] Wxh = new float[numberInputs + 1, numberHidden];
             float[,] Why = new float[numberHidden + 1, numberOutputs];
-            int t, i, h, o;
+
+            List<List<float>> inputs = new List<List<float>>();
+            List<List<float>> outputs = new List<List<float>>();
+
+            List<float> temp = new List<float>();
+
+            temp.Add(2);
+            temp.Add(4);
+
+            inputs.Add(temp.ToList());
+            temp.Clear();
+
+            
+            temp.Add(5);
+            temp.Add(6);
+
+            inputs.Add(temp.ToList());
+            temp.Clear();
+
+           
+            temp.Add(7);
+            temp.Add(3);
+
+            inputs.Add(temp.ToList());
+            temp.Clear();
+
+            
+            temp.Add(-2);
+            temp.Add(-4);
+
+            inputs.Add(temp.ToList());
+            temp.Clear();
+
+            temp.Add(1);
+            temp.Add(0);
+            outputs.Add(temp.ToList());
+            temp.Clear();
+
+            temp.Add(1);
+            temp.Add(0);
+            outputs.Add(temp.ToList());
+            temp.Clear();
+
+            temp.Add(1);
+            temp.Add(0);
+            outputs.Add(temp.ToList());
+            temp.Clear();
+
+            temp.Add(0);
+            temp.Add(1);
+            outputs.Add(temp.ToList());
+            temp.Clear();
 
 
             for (i = 0; i < numberInputs + 1; i++)
+            {
+                List<float> tmp = new List<float>();
                 for (int j = 0; j < numberHidden; j++)
                 {
                     Wxh[i, j] = Convert.ToSingle(random.NextDouble() * (0.1 + 0.1) - 0.1);
                     Thread.Sleep(1);
                 }
+
+            }
             for (i = 0; i < numberHidden + 1; i++)
+            {
+                List<float> tmp = new List<float>();
                 for (int j = 0; j < numberOutputs; j++)
                 {
                     Why[i, j] = Convert.ToSingle(random.NextDouble() * (0.1 + 0.1) - 0.1);
                     Thread.Sleep(1);
                 }
 
-            float loss, sum; //for storing the loss
-
-
-            float[] x = new float[numberInputs+1];
-            float[] y = new float[numberOutputs];
-            float[] zhWeightedSums = new float[numberHidden];
-            float[] hActivationValues = new float[numberHidden+1];
-            float[] zyWeightedSums = new float[numberOutputs];
-            float[] probabilities = new float[numberOutputs];
-            float[] outputErrors = new float[numberOutputs];
-            float[,] deltaWxh = new float[numberInputs+1, numberHidden];
-            float[,] deltaWhy = new float[numberHidden+1, numberOutputs];
-
-
-
-
-
-            for (t = 0; t < iterations; t++)
-            {
-
-                int counter = 0;
-                List<String> dates = new List<String>();
-                List<double> inputsList = new List<double>();
-                String outputList = "";
-                string line;
-                int numberFile = t % numberTraining;
-                string fileName = numberFile.ToString() + ".txt";
-                
-                // Read the file and display it line by line.
-                System.IO.StreamReader file =
-                    new System.IO.StreamReader(@fileName);
-                while ((line = file.ReadLine()) != null)
-                {
-                    if (counter % 2 == 0 && counter != 10)
-                    {
-                        dates.Add(line);
-                    }
-                    else if (counter % 2 == 1)
-                        inputsList.Add(double.Parse(line));
-                    else
-                        outputList = line;
-                    
-                    counter++;
-                }
-
-                file.Close();
-
-                for (i = 1; i < numberInputs; i++)
-                    x[i] = Convert.ToSingle(inputsList[i - 1]);
-                x[0] = 1;
-
-                for (i = 0; i < numberOutputs; i++)
-                    y[i] = outputList[i] - 48;
-
-
-
-                for (i = 0; i < numberHidden; i++)
-                    zhWeightedSums[i] = 0;
-                for (h = 0; h < numberHidden; h++) for (i = 0; i < numberHidden + 1; i++) zhWeightedSums[h] += x[i] * Wxh[i, h];
-
-                hActivationValues[0] = 1;
-                for (h = 0; h < numberHidden; h++) hActivationValues[h + 1] = Convert.ToSingle(Math.Tanh(zhWeightedSums[h]));
-                
-                for (i = 0; i < numberOutputs; i++)
-                    zyWeightedSums[i] = 0;
-
-                for (o = 0; o < numberOutputs; o++) for (h = 0; h < numberHidden + 1; h++) zyWeightedSums[o] += hActivationValues[h] * Why[h, o];
-
-                for (sum = 0, o = 0; o < numberOutputs; o++) { probabilities[o] = Convert.ToSingle(Math.Exp(zyWeightedSums[o])); sum += probabilities[o]; }
-
-                Console.WriteLine(numberFile + ": ");
-                for (o = 0; o < numberOutputs; o++)
-                    Console.Write(probabilities[o] + " ");
-                Console.WriteLine();
-                for (o = 0; o < numberOutputs; o++)
-                    Console.Write(y[o] + " ");
-
-                    for (o = 0; o < numberOutputs; o++) outputErrors[o] = probabilities[o] - y[o];
-
-                for (loss = 0, o = 0; o < numberOutputs; o++) loss -= y[o] * Convert.ToSingle(Math.Log(probabilities[o]));
-                Console.WriteLine();
-                Console.WriteLine("Loss: " + loss);
-
-
-
-                for (h = 0; h < numberHidden + 1; h++) for (o = 0; o < numberOutputs; o++) deltaWhy[h, o] = hActivationValues[h] * outputErrors[o];
-
-
-                for (i = 0; i < numberInputs + 1; i++)
-                    hActivationValues[i] = 0;
-
-                for (h = 1; h < numberHidden + 1; h++) for (o = 0; o < numberOutputs; o++) hActivationValues[h] += Why[h, o] * outputErrors[o];
-
-                for (h = 0; h < numberHidden; h++) zhWeightedSums[h] = hActivationValues[h + 1] * (1 - Convert.ToSingle(Math.Pow(Math.Tanh(zhWeightedSums[h]), 2)));
-
-
-                for (i = 0; i < numberInputs + 1; i++) for (h = 0; h < numberHidden; h++) deltaWxh[i, h] = x[i] * zhWeightedSums[h];
-
-
-                for (h = 0; h < numberHidden + 1; h++) for (o = 0; o < numberOutputs; o++) Why
-                [h, o] -= learningrate * deltaWhy[h, o];
-                for (i = 0; i < numberInputs + 1; i++) for (h = 0; h < numberHidden; h++) Wxh[i, h] -= learningrate * deltaWxh[i, h];
-
-
             }
 
-            Console.ReadKey();
+            Network myNetwork = new Network(numberInputs, numberOutputs, numberHidden, numberTraining, inputs, outputs, Wxh, Why);
+            myNetwork.learning(1000000, false);
 
+            List<List<float>> probabilities = myNetwork.estimate(inputs);
+        
         }
 
     }
